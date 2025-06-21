2236b7e0afe320756cd8475fba1f7886eef44475
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-06-22 03:06:13 +0530

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -1,5 +1,10 @@
 def call(String env) {
-    bat "echo Initializing Terraform for ${env}"
-    bat "terraform -chdir=infra/${env} init"
-    bat "terraform -chdir=infra/${env} apply -var-file=${env}.tfvars -auto-approve"
+    withCredentials([[
+        $class: 'AmazonWebServicesCredentialsBinding',
+        credentialsId: 'aws-ecr-creds'
+    ]]) {
+        bat "echo Initializing Terraform for ${env}"
+        bat "terraform -chdir=infra/${env} init"
+        bat "terraform -chdir=infra/${env} apply -var-file=${env}.tfvars -auto-approve"
+    }
 }
