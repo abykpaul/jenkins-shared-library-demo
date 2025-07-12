71b89afb0d580a6d171a68f43994dc0d7d8eaecf
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-07-12 22:27:12 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,113 +1,36 @@
 def call(Map config) {
-  pipeline {
-    agent any
-
-    environment {
-      AWS_REGION = config.region
-      IMAGE_NAME = config.imageName
-      REPO_NAME  = config.repoName
-    }
-def call(Map config) {
-  pipeline {
-    agent any
-
-    environment {
-      AWS_REGION = 'ap-south-1'  // Default or hardcoded, can't use config.xxx directly here
+  node {
+    stage('Init Vars') {
+      env.REPO_NAME  = "${config.repoName}"
+      env.IMAGE_NAME = "${config.imageName}"
+      env.GIT_URL    = "${config.gitUrl}"
+      env.AWS_CREDS  = "${config.awsCredsId}"
+      env.AWS_REGION = "${config.region}"
     }
 
-    stages {
-      stage('Init Vars') {
-        steps {
-          script {
-            env.REPO_NAME  = config.repoName
-            env.IMAGE_NAME = config.imageName
-            env.GIT_URL    = config.gitUrl
-            env.AWS_CREDS  = config.awsCredsId
-          }
-        }
-      }
+    stage('Checkout App') {
+      git url: env.GIT_URL
+    }
 
-      stage('Checkout App') {
-        steps {
-          git url: env.GIT_URL
-        }
-      }
+    stage('Docker Build') {
+      bat "docker build -t %REPO_NAME%:latest ."
+    }
 
-      stage('Docker Build') {
-        steps {
-          bat "docker build -t %REPO_NAME%:latest ."
-        }
-      }
-
-      stage('Login to ECR') {
-        steps {
-          withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: env.AWS_CREDS]]) {
-            bat """
-              for /f %%i in ('aws sts get-caller-identity --query "Account" --output text') do set ACCOUNT_ID=%%i
-              aws ecr get-login-password --region %AWS_REGION% | docker login --username AWS --password-stdin %ACCOUNT_ID%.dkr.ecr.%AWS_REGION%.amazonaws.com
-            """
-          }
-        }
-      }
-
-      stage('Tag & Push Image') {
-        steps {
-          bat """
-            for /f %%i in ('aws sts get-caller-identity --query "Account" --output text') do set ACCOUNT_ID=%%i
-            docker tag %REPO_NAME%:latest %%ACCOUNT_ID%%.dkr.ecr.%AWS_REGION%.amazonaws.com/%REPO_NAME%:latest
-            docker push %%ACCOUNT_ID%%.dkr.ecr.%AWS_REGION%.amazonaws.com/%REPO_NAME%:latest
-          """
-        }
+    stage('Login to ECR') {
+      withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: env.AWS_CREDS]]) {
+        bat """
+          FOR /F %%i IN ('aws sts get-caller-identity --query "Account" --output text') DO SET ACCOUNT_ID=%%i
+          aws ecr get-login-password --region %AWS_REGION% | docker login --username AWS --password-stdin %ACCOUNT_ID%.dkr.ecr.%AWS_REGION%.amazonaws.com
+        """
       }
     }
-  }
-}
-    stages {
-      stage('Checkout App') {
-        steps {
-          git url: config.gitUrl
-        }
-      }
 
-      stage('Docker Build') {
-        steps {
-          script {
-            bat "docker build -t %REPO_NAME%:latest ."
-          }
-        }
-      }
-
-      stage('Login to ECR') {
-        steps {
-          withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: config.awsCredsId]]) {
-            script {
-              def accountId = bat(
-                script: 'aws sts get-caller-identity --query "Account" --output text',
-                returnStdout: true
-              ).trim()
-
-              def repoUri = "${accountId}.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
-
-              bat """
-                for /f %%i in ('aws sts get-caller-identity --query "Account" --output text') do set ACCOUNT_ID=%%i
-                aws ecr get-login-password --region %AWS_REGION% | docker login --username AWS --password-stdin %ACCOUNT_ID%.dkr.ecr.%AWS_REGION%.amazonaws.com
-              """
-            }
-          }
-        }
-      }
-
-      stage('Tag & Push Image') {
-        steps {
-          script {
-            bat """
-              for /f %%i in ('aws sts get-caller-identity --query "Account" --output text') do set ACCOUNT_ID=%%i
-              docker tag %REPO_NAME%:latest %%ACCOUNT_ID%%.dkr.ecr.%AWS_REGION%.amazonaws.com/%REPO_NAME%:latest
-              docker push %%ACCOUNT_ID%%.dkr.ecr.%AWS_REGION%.amazonaws.com/%REPO_NAME%:latest
-            """
-          }
-        }
-      }
+    stage('Tag & Push Image') {
+      bat """
+        FOR /F %%i IN ('aws sts get-caller-identity --query "Account" --output text') DO SET ACCOUNT_ID=%%i
+        docker tag %REPO_NAME%:latest %%ACCOUNT_ID%%.dkr.ecr.%AWS_REGION%.amazonaws.com/%REPO_NAME%:latest
+        docker push %%ACCOUNT_ID%%.dkr.ecr.%AWS_REGION%.amazonaws.com/%REPO_NAME%:latest
+      """
     }
   }
 }
